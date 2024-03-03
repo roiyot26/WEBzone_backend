b6ff948b93c828efd673acffe59827bb6d4ffa3e
roiyot26 <95028994+roiyot26@users.noreply.github.com>
2024-03-03 20:39:50 +0200

    Update prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
   // dbURL: 'mongodb+srv://noy96:webzone22@webzone.ksxsd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  dbURL:'mongodb+srv://roiyot26:MAC1oDKw1VjCji5l@cluster0.hwpnvmk.mongodb.net/'
+  dbURL:'mongodb+srv://roiyot26:MAC1oDKw1VjCji5l@cluster0.hwpnvmk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 }
